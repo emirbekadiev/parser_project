5e2b22661eb86acdbc3aa2f19977a5d45ca32ffd
emirbekadiev <adiev.emirbek10@gmail.com>
2025-03-13 20:05:46 +0600

    last commit

--- get_links.py
+++ get_links.py
@@ -2,6 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 import json
+import os
+
+if not os.path.exists('data'):
+  os.mkdir('data')
 
 URL = 'https://m.mashina.kg/search/all/'
 
@@ -16,6 +20,6 @@ for page in tqdm(range(1, num_pages+1)):
   for block_image in soup.find_all('div', class_='list-item list-label'):
     sub_urls.append(f"https://m.mashina.kg{block_image.find('a')['href']}")
 
-with open('links.json', 'w') as f:
+with open('data/links.json', 'w') as f:
     json.dump(sub_urls, f, indent=4)
 
